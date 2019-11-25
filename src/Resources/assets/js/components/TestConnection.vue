76b3b169f193c1c4d50c84ca958a40141018f5d7
darek <dariusz.mecinski@gmail.com>
2019-11-25 16:26:54 +0100

    refactor

---
+++ src/Resources/assets/js/components/TestConnection.vue
@@ -0,0 +1,28 @@
+<template>
+<span>
+    <slot>
+        <el-button>Test Connection</el-button>
+    </slot>
+</span>
+</template>
+
+<script>
+import {
+    Button,
+    ButtonGroup
+} from 'element-ui';
+
+export default {
+    props: {
+
+    },
+    data() {
+        return {
+
+        }
+    },
+    mounted() {
+
+    }
+};
+</script>
